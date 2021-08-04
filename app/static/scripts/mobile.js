152384e754ad7a16f7d9d7e16fb7c02777e46b67
skaylan <lucas_7tv@hotmail.com>
2021-08-03 21:51:03 -0300

    add header to base.html and javascript to mobible reponsive

---
+++ app/static/scripts/mobile.js
@@ -0,0 +1,8 @@
+const btn_mobile = document.getElementById('btn-mobile');
+
+function toggle_menu(event){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btn_mobile.addEventListener('click', toggle_menu);
